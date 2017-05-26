4ead85a0352d2ff814f3992be44fc30785c2b9b8
Demo-du <1556141770@qq.com>
2017-05-26 14:35:24 +0800

    526

---
+++ src/leetcode/n228.java
@@ -0,0 +1,24 @@
+package leetcode;
+/*
+Given a sorted integer array without duplicates, return the summary of its ranges.
+
+For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
+
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+
+Subscribe to see which companies asked this question.
+
+ */
+import java.util.List;
+
+public class n228 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public static List<String> summaryRanges(int[] nums) {
+        return null;
+    }
+}

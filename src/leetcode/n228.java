662194f828dbebc651753be750549af32f381a77
Demo-du <1556141770@qq.com>
2017-05-27 10:23:56 +0800

    527

--- src/leetcode/n228.java
+++ src/leetcode/n228.java
@@ -1,4 +1,5 @@
 package leetcode;
+import java.util.ArrayList;
 /*
 Given a sorted integer array without duplicates, return the summary of its ranges.
 
@@ -16,9 +17,27 @@ public class n228 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        int[] nums={0,1,2,4,5,7};
+        System.out.println(summarRanges(nums));
+        //summaryRanges
 	}
-    public static List<String> summaryRanges(int[] nums) {
-        return null;
+    public static List<String> summarRanges(int[] nums) {
+    	List<String> list=new ArrayList();
+    	if(nums.length==1){
+    		list.add(nums[0]+"");
+    		return list;
+    	}
+        for(int i=0;i<nums.length;i++){
+        	int a=nums[i];
+        	while(i+1<nums.length&&(nums[i+1]-nums[i])==1){
+        		i++;
+        	}
+        	if(a!=nums[i]){
+        		list.add(a+"->"+nums[i]);
+        	}else{
+        		list.add(a+"");
+        	}
+        }
+        return list;
     }
 }

bf3b9fb3af2f24ff1cd61bd3fb6269caeb052635
Demo-du <1556141770@qq.com>
2017-06-01 21:34:28 +0800

    6-01

---
+++ src/leetcode/TreeNode.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class TreeNode {
+    int value;
+    TreeNode left_Node;
+    TreeNode right_Node;
+    public TreeNode(int value){
+    	this.value=value;
+    	this.left_Node=null;
+    	this.right_Node=null;
+    }
+}

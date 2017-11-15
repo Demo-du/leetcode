a35b3de601b9da0a432dac315a6a67a825c36382
Demo-du <1556141770@qq.com>
2017-11-15 20:24:27 +0800

    罗马数字转为整数

--- src/leetcode/n13.java
+++ src/leetcode/n13.java
@@ -1,19 +1,51 @@
 package leetcode;
 
 public class n13 {
-	public static String s="aaa";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ad d=new ad();
-		ss(ad.e);
-		System.out.println(ad.e);
-
+        n13 n=new n13();
+        System.out.println(n.romanToInt(""));
 	}
-	public static void ss(String a){
-		a="ddd";
-	}
-
-}
-class ad{
-	public static String e="abd";
+    public int romanToInt(String s) {
+    	int res=chartoint(s.charAt(0));
+    	int pre=0;
+    	int now=1;
+    	for(int i=1;i<s.length();i++){
+    		pre=chartoint(s.charAt(i-1));
+    		now=chartoint(s.charAt(i));
+    		if(pre>=now){
+    			res+=now;
+    		}else{
+    			res=res-pre+now-pre;
+    		}
+    	}
+        return res;
+    }
+    public int chartoint(char a){
+    	int data=0;
+    	switch(a){
+	    	case 'I':  
+	            data = 1;  
+	            break;  
+	        case 'V':  
+	            data = 5;  
+	            break;  
+	        case 'X':  
+	            data = 10;  
+	            break;  
+	        case 'L':  
+	            data = 50;  
+	            break;  
+	        case 'C':  
+	            data = 100;  
+	            break;  
+	        case 'D':  
+	            data = 500;  
+	            break;  
+	        case 'M':  
+	            data = 1000;  
+	            break;  
+    	}
+    	return data;
+    }
 }

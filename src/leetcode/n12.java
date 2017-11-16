69ba3f4e463ee1e7a36706d28b4b27d9910df185
Demo-du <1556141770@qq.com>
2017-11-16 14:56:17 +0800

    Integer to roma

--- src/leetcode/n12.java
+++ src/leetcode/n12.java
@@ -31,7 +31,7 @@ public class n12 {
     	shu.add(10);
     	shu.add(50);
     	shu.add(500);*/
-    	dic.put(1, "I");
+    	/*dic.put(1, "I");
     	dic.put(5, "V");
     	dic.put(10, "X");
     	dic.put(50, "L");
@@ -40,8 +40,28 @@ public class n12 {
     	dic.put(1000, "M");
     	String ans="";
     	int temp=0;
-    	temp=num/1000;
-    	
-        return null;
+    	temp=num/1000;*/
+    	if(num>=1000){
+    		return "M"+intToRoman(num-1000);
+    	}
+    	if(num>=900){
+    		return "CM"+intToRoman(num-900);
+    	}
+    	if(num>=500){
+    		return "D"+intToRoman(num-500);
+    	}
+    	if(num>=400){
+    		return "CD"+intToRoman(num-400);
+    	}
+    	if(num>=100) return "C"+intToRoman(num-100);
+    	if(num>=90) return "XC"+intToRoman(num-90);
+    	if(num>=50) return "L"+intToRoman(num-50);
+    	if(num>=40) return "XL"+intToRoman(num-40);
+    	if(num>=10) return "X"+intToRoman(num-10);
+    	if(num>=9) return "IX"+intToRoman(num-9);
+    	if(num>=5) return "V"+intToRoman(num-5);
+    	if(num>=4) return "IV"+intToRoman(num-4);
+    	if(num>=1) return "I"+intToRoman(num-1);
+        return "";
     }
 }

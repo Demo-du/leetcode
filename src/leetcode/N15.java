b51ea9e2340bdd718f1c5324c6b9473bfe715155
Demo-du <1556141770@qq.com>
2017-11-16 15:43:19 +0800

    3Sum

--- src/leetcode/N15.java
+++ src/leetcode/N15.java
@@ -1,4 +1,7 @@
 package leetcode;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 /*
  * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
@@ -20,7 +23,54 @@ public class N15 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        N15 n15=new N15();
+        int []nums={0,0,0,0};
+        System.out.println(n15.threeSum(nums));
 	}
+    public List<List<Integer>> threeSum(int[] nums) {
+    	Arrays.sort(nums);
+    	List<List<Integer>> total=new LinkedList<List<Integer>>();
+    	for(int i=0;i<nums.length;i++){
+    		if(i>0&&nums[i]==nums[i-1]){
+    			continue;
+    		}
+    		//System.out.println("ss");
+    		List<List<Integer>> totaltemp=find(i,nums,i+1,nums.length-1);
+    		for(List<Integer> temp:totaltemp){
+    			//System.out.println(temp);
+    			if(temp.size()>0){
+ 	    		   total.add(temp);
+ 	    		   
+    			}
+    				
 
+    			
+    		}
+    	}
+        return total;
+    }
+    public List<List<Integer>> find(int i,int[] nums,int index1,int index2){
+    	List<Integer> newList=new LinkedList<Integer>();
+    	List<List<Integer>> total=new LinkedList<List<Integer>>();
+    	while(index1<index2){
+    		//System.out.println("ss");
+    		if(nums[index1]+nums[index2]+nums[i]>0){
+    			index2--;
+    		}else if(nums[index1]+nums[index2]+nums[i]<0){
+    			index1++;
+    		}else{
+    			//System.out.println(nums[i]);
+    			newList=new LinkedList<Integer>();
+    			newList.add(nums[i]);
+    			newList.add(nums[index1]);
+    			newList.add(nums[index2]);
+    			total.add(newList);
+                index1++;
+                while(nums[index1]==nums[index1-1]&&index1<index2){
+                	index1++;
+                }
+    		}
+    	}
+    	return total;
+    }
 }

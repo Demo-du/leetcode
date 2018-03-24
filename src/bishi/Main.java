fa24bb27635bf15395fc1e1837c33156f28c0674
Demo-du <1556141770@qq.com>
2018-03-24 21:08:28 +0800

    头条笔试

--- src/bishi/Main.java
+++ src/bishi/Main.java
@@ -1,5 +1,61 @@
-package bishi;
+package toutiao;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()) {
+        	int n=sc.nextInt();
+        	int k=sc.nextInt();
+        	int []nums=new int[n];
+        	for(int i=0;i<n;i++) {
+        		nums[i]=sc.nextInt();
+        	}
+        	Arrays.sort(nums);
+        	HashSet<Integer> h=new HashSet<Integer>();
+        	for(int i=0;i<n;i++) {
+        		h.add(nums[i]);
+        	}
+        	nums=new int[h.size()];
+        	n=h.size();
+        	int tt=0;
+        	Iterator it=(Iterator) h.iterator();
+        	//
+        	int index1=0;
+        	int index2=0;
+        	int cnt=0;
+        	while(index2<n) {
+        		if(nums[index2]-nums[index1]>k) {
+        			index1++;
+        		}else if(nums[index2]-nums[index1]<k) {
+        			index2++;
+        		}else {
+        			cnt++;
+        			index1++;
+            		index2++;
+        		}
+        		
+        		/*while(index1<n&&index2<n&&nums[index1+1]==nums[index1]) {
+        			index1++;
+        		}
+        		while(index1<n&&index2<n&&nums[index1+1]==nums[index1]) {
+        			index1++;
+        		}*/
+        	}
+        	System.out.println(cnt);
+        }
+	}
+
+}
+/*
+ * *import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,38 +64,33 @@ public class Main {
 		// TODO Auto-generated method stub
         Scanner sc=new Scanner(System.in);
         while(sc.hasNext()) {
-        	String a=sc.next();
-        	String b=sc.next();
-        	int num=0;
-        	char []L;
-        	char[]S;
-        	if(a.length()>b.length()) {
-        		L=a.toCharArray();
-        		S=b.toCharArray();
-        	}else {
-        		S=a.toCharArray();
-        		L=b.toCharArray();
+        	int n=sc.nextInt();
+        	int k=sc.nextInt();
+        	int []nums=new int[n];
+        	for(int i=0;i<n;i++) {
+        		nums[i]=sc.nextInt();
         	}
-        	int index=0;
+        	Arrays.sort(nums);
         	int index1=0;
         	int index2=0;
-        	if(S.length==0||L.length==0) {
-        		num=0;
-        	}else while(index<L.length-S.length+1) {
-        		while(index1<L.length&&index2<S.length) {
-            		if(L[index1]!=S[index2]) {
-            			//System.out.println(L[index1]);
-            			num++;
-            		}
-            		index1++;
+        	int cnt=0;
+        	while(index2<n) {
+        		if(nums[index2]-nums[index1]>k) {
+        			index1++;
+        		}else if(nums[index2]-nums[index1]<k) {
+        			index2++;
+        		}else {
+        			cnt++;
+        			index1++;
             		index2++;
-            	}
-        		index++;
-        		index1=index;
-        		index2=0;
+        		}
+        		
+        		
         	}
-        	System.out.println(num);
+        	System.out.println(cnt);
         }
 	}
 
 }
+
+ */
